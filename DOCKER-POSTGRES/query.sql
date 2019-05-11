ca5e242e8f1b0b5fab57d685303b71728927f06b
Francisco <fco.olivaress@gmail.com>
2019-05-11 18:08:06 -0400

    Charge the new unified csv

--- DOCKER-POSTGRES/query.sql
+++ DOCKER-POSTGRES/query.sql
@@ -33,10 +33,11 @@ VALUES
 (250, 'Anderson', 'Jane', DEFAULT);
 
 /* OBTIENE LAS MEDICIONES CON INTERIOR Y EXTERIOR */
+/* Nota: hay unos registros que solo registraron el ruido al interior de la casa, entonces por eso se filtra que la temp interior no sea nula para que esos no aparezcan*/
 INSERT into "mediciones_netatmo_unificadas" (dispositivo_id, vivienda_id, temperatura_interior, humedad_interior, co2, ruido, temperatura_exterior, humedad_exterior, measured_at, date_measured_at)
 SELECT interior.dispositivo_id, interior.vivienda_id, interior.temperatura as temperatura_interior, interior.humedad as humedad_interior, 
 interior.co2, interior.ruido, exterior.temperatura as temperatura_exterior, exterior.humedad as humedad_exterior, interior.measured_at, interior.date_measured_at
 FROM mediciones_netatmo interior
 INNER JOIN mediciones_netatmo exterior ON ((interior.measured_at = exterior.measured_at) AND (interior.vivienda_id = exterior.vivienda_id))
-WHERE interior.sector = FALSE AND exterior.sector = TRUE
+WHERE interior.sector = FALSE AND exterior.sector = TRUE AND interior.temperatura is not NULL
 ORDER BY date_measured_at ASC;
